85f233fc1035d926a7d0e88784186f5270dd87f5
Andrew <andrewkafrolov76@gmail.com>
2026-04-07 13:45:07 +0300

    some changes

--- 2-course/SEM4/Komp Mod/Вращение/rotation.py
+++ 2-course/SEM4/Komp Mod/Вращение/rotation.py
@@ -1,25 +1,10 @@
 from math import sqrt
 
-# A = [[5, 7, 6, 5], [7, 10, 8, 7], [6, 8, 10, 9], [5, 7, 9, 10]]
-# B = [23, 32, 33, 31]
+A = [[5, 7, 6, 5], [7, 10, 8, 7], [6, 8, 10, 9], [5, 7, 9, 10]]
+B = [23, 32, 33, 31]
 
-A = [[4, 2, -1, 3], [3, 6, 2, -1], [2, -1, 5, 4], [1, 3, 2, 7]]
-
-B = [6, 5, 24, 12]
-
-
-def printMatrix(A):
-    print("---------Матрица А---------\n")
-
-    for i in range(len(A)):
-        for j in range(len(A[i])):
-            if j == len(A):
-                print("B: ", round(A[i][j], 1), end=" ")
-            else:
-                print(round(float(A[i][j]), 1), end=" | ")
-        print("\n")
-
-    print("---------------------------")
+# [[4, 2, -1, 3], [3, 6, 2, -1], [2, -1, 5, 4], [1, 3, 2, 7]]
+# [6, 5, 24, 12]
 
 
 def rotation(A, B) -> list:
@@ -53,4 +38,4 @@ def rotation(A, B) -> list:
 
 X = rotation(A, B)
 for x in X:
-    print(round(x, 1))
+    print(round(x, 1), end="\t")
